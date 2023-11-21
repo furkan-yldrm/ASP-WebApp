4dada1d5225e2d8b61369131fe3ea5bd3e1e8ea9
Furkan Yıldırım <yildirim.frkn@outlook.com>
2023-11-21 18:50:14 +0300

    department list some attempts

---
+++ ASP-WebApp/ASP-WebApp/Controllers/DepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using ASP_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace ASP_WebApp.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var values = c.Departments.ToList();
+            return View(values);
+        }
+    }
+}
